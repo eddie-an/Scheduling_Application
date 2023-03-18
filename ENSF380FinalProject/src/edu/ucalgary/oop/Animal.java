a048b1b5d8a4cedbff28283797776bd291442b07
karambaroud <113051905+karambaroud@users.noreply.github.com>
2023-03-18 16:24:54 -0600

    Added documentation to Animal class

--- ENSF380FinalProject/src/edu/ucalgary/oop/Animal.java
+++ ENSF380FinalProject/src/edu/ucalgary/oop/Animal.java
@@ -1,3 +1,15 @@
+/**
+@author     Group5
+@version    1.4
+@since      1.0
+*/
+
+/*
+ * Animal is an abstract class for the subclasses Fox, Beaver, Porcupine, Coyote, Raccoon
+ * with info about the Animal ID, animal name, and whether or not the animal is an orphan.
+*/
+
+
 package edu.ucalgary.oop;
 
 public abstract class Animal {
@@ -5,30 +17,53 @@ public abstract class Animal {
     private String name;
     private boolean isOrphaned;
 
+
+    /**
+     * This is the constructor for the Animal class.
+     * @param id            The ID of the animal.
+     * @param name          The name of the animal.
+     * @param isOrphaned    Whether or not the animal is an orphan.
+     */
     public Animal(int id, String name, boolean isOrphaned) {
         this.ANIMAL_ID = id;
         this.name = name;
         this.isOrphaned = isOrphaned;
     }
 
+    
+    /**
+     * @return  The ID of the animal.
+     */
     public int getAnimalID() {
         return this.ANIMAL_ID;
     }
 
+    /**
+     * @return  The name of the animal.
+     */
     public String getName() {
         return this.name;
     }
 
+    /**
+     * @return  Whether or not the animal is an orphan.
+     */
     public boolean getOrphanStatus() {
         return this.isOrphaned;
     }
 
+    /**
+     * @return  The active time of the animal.
+     */
     public abstract String getActiveTime();
 
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * @param isOrphaned    Whether or not the animal is an orphan.
+     */
     public void setOrphanStatus(boolean isOrphaned) {
         this.isOrphaned = isOrphaned;
     }
