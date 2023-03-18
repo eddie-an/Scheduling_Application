c8388d5dcb209b3b7775b971e9ecf30b4f270cf5
karambaroud <113051905+karambaroud@users.noreply.github.com>
2023-03-18 16:59:19 -0600

    Added documentation to App class

--- ENSF380FinalProject/src/edu/ucalgary/oop/App.java
+++ ENSF380FinalProject/src/edu/ucalgary/oop/App.java
@@ -1,7 +1,36 @@
+/**
+@author     Group5
+@version    1.0
+@since      1.0
+*/
+
+/*
+ * App is the class that contains the main method. It is the entry point of the program.
+ * App will have a GUI that will allow the user to interact with the program for these purposes:
+ *     - The program should also display all scheduling information and require a confirmation from the user for
+ *       each instance that the backup volunteer needs to be contacted.
+ * 
+ *     - Users should be provided with meaningful error messages appropriate to the audience
+ *       (end user, rather than programmer). Error messages must explain what was wrong and
+ *       how it can be corrected (e.g., "It was impossible to complete the schedule due to too
+ *       many events scheduled at 11 AM. Please shift some of the following activities:
+ *       rebandage head wound (Spike), administer antibiotics (Spike, Shadow)."
+ * 
+ *     - Users should be given the opportunity to correct their mistakes, and opportunities for
+ *       mistakes should be limited. 
+*/
+
 package edu.ucalgary.oop;
 
 public class App {
+
+    /**
+     * This is the main method of the program.
+     * @param args  The command line arguments.
+     * @throws Exception    If an exception is thrown.
+     */
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
 }
+
