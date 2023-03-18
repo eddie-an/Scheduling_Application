6a80cb94ecdf94a91b6dcf49a936829f02646a15
Alphamale42 <111829951+Alphamale42@users.noreply.github.com>
2023-03-18 17:01:25 -0600

    Update Task.java

--- ENSF380FinalProject/src/edu/ucalgary/oop/Task.java
+++ ENSF380FinalProject/src/edu/ucalgary/oop/Task.java
@@ -2,23 +2,30 @@ package edu.ucalgary.oop;
 
 import java.time.LocalDate;
 
-import edu.ucalgary.oop.Animal;
+
 
 public class Task {
     private LocalDate startTime;
     private int prepTime;
     private int taskTime;
     private String taskType;
-    private Volunteer assignedVolunteer;
+    private VolunteerGeneric<String, Integer> volunteers;  // object binded to Task object
     private Animal animalTaskFor;
 
-    public Task(LocalDate startTime, int prepTime, int taskTime, String taskType, Volunteer volunteer, Animal animal) {
-        this.startTime = startTime;
-        this.prepTime = prepTime;
-        this.taskTime = taskTime;
-        this.taskType = taskType;
-        this.assignedVolunteer = volunteer;
-        this.animalTaskFor = animal;
+    public Task(LocalDate startTime, int prepTime, int taskTime, String taskType, String volunteerId, Animal animal) {
+        if(prepTime + taskTime > 60) {
+            throw new ExtraVolunterException();   // deal with this later, this exception handles Task object creation
+        }
+        else {
+            Integer tempInt = 60;
+            this.startTime = startTime;
+            this.prepTime = prepTime;
+            this.taskTime = taskTime;
+            this.taskType = taskType;
+            this.volunteers = new VolunteerGeneric<String, Integer>(volunteerId, tempInt);
+            this.animalTaskFor = animal;
+        }
+
     }
 
     public LocalDate getStartTime() {
@@ -42,7 +49,27 @@ public class Task {
     }
 
     public Animal getAnimal() {
-
+        return this.animalTaskFor;
     }
+
+}
+
+class VolunteerGeneric<K, T> {   // this class is binded strictly to the Task class, during all instances of Task.
+    private K volunteerID;
+    private T timeLeft;
+    
+    public VolunteerGeneric (K first, T second) {
+        this.volunteerID = first;
+        this.timeLeft = second;
+    }
+
+    public T getTimeLeft(){
+        return timeLeft;
+    }
+
+    public K getVolunteerID() {
+        return volunteerID;
+    }
+
 
 }
