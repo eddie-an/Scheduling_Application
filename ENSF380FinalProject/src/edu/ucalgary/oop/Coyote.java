65874256c928b4aac3726a7b144d10e3745f7995
karambaroud <113051905+karambaroud@users.noreply.github.com>
2023-03-18 16:34:58 -0600

    Added documentation for Coyote class

--- ENSF380FinalProject/src/edu/ucalgary/oop/Coyote.java
+++ ENSF380FinalProject/src/edu/ucalgary/oop/Coyote.java
@@ -1,33 +1,68 @@
+/**
+@author     Group5
+@version    1.4
+@since      1.0
+*/
+
+/*
+ * Coyote is a class which extends the Animal class and represents a coyote.
+ * It has has a static field ACTIVE_TIME which is set to "crepescular".
+*/
+
 package edu.ucalgary.oop;
 
 public class Coyote extends Animal {
     private final static String ACTIVE_TIME = "crepuscular";
 
+    /**
+     * This is the constructor for the Coyote class.
+     * @param id            The ID of the animal.
+     * @param name          The name of the animal.
+     * @param isOrphaned    Whether or not the animal is an orphan.
+     */
     public Coyote(int id, String name, boolean isOrphaned) {
         super(id, name, isOrphaned);
     }
 
+    /**
+     * @return  The ID of the animal.
+     */
     public int getAnimalID() {
         return super.getAnimalID();
     }
 
+    /**
+     * @return  The name of the animal.
+     */
     public String getName() {
         return super.getName();
     }
 
+    /**
+     * @return  Whether or not the animal is an orphan.
+     */
     public boolean getOrphanStatus() {
         return super.getOrphanStatus();
     }
 
+    /**
+     * @return  The active time of the animal.
+     */
     @Override
     public String getActiveTime() {
         return ACTIVE_TIME;
     };
 
+    /**
+     * @param name  The name of the animal.
+     */
     public void setName(String name) {
         super.setName(name);
     }
 
+    /**
+     * @param isOrphaned  Whether or not the animal is an orphan.
+     */
     public void setOrphanStatus(boolean isOrphaned) {
         super.setOrphanStatus(isOrphaned);
     }
