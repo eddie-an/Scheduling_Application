12397aa3b50f8c55b735271c7e5b4cf6fe275a8e
Alphamale42 <111829951+Alphamale42@users.noreply.github.com>
2023-03-18 17:19:02 -0600

    Update Task.java

--- ENSF380FinalProject/src/edu/ucalgary/oop/Task.java
+++ ENSF380FinalProject/src/edu/ucalgary/oop/Task.java
@@ -9,12 +9,29 @@ public class Task {
     private int prepTime;
     private int taskTime;
     private String taskType;
-    private VolunteerGeneric<String, Integer> volunteers;  // object binded to Task object
+    private VolunteerGeneric<String, Integer> volunteerOne;  // object binded to Task object
+    private VolunteerGeneric<String, Integer> volunteerTwo;  // On - standby in case it needs to be used / accessed.
     private Animal animalTaskFor;
 
     public Task(LocalDate startTime, int prepTime, int taskTime, String taskType, String volunteerId, Animal animal) {
         if(prepTime + taskTime > 60) {
+            
+        // Another option: 
+            
             throw new ExtraVolunterException();   // deal with this later, this exception handles Task object creation
+            
+        //  The above would essentially re-direct the task class to another class which would handle initialization of all Task data members and second volunteer,
+        //  although this method seems a bit redundant...
+        // One option below :
+            Integer tempInt = 60;
+            this.startTime = startTime;
+            this.prepTime = prepTime;
+            this.taskTime = taskTime;
+            this.taskType = taskType;
+            this.volunteerOne = new VolunteerGeneric<String, Integer>(volunteerId, tempInt);
+            this.volunteerTwo = new VolunteerGeneric<String, Integer>(volunteerId + 1, tempInt);
+            this.animalTaskFor = animal;
+    
         }
         else {
             Integer tempInt = 60;
@@ -22,7 +39,7 @@ public class Task {
             this.prepTime = prepTime;
             this.taskTime = taskTime;
             this.taskType = taskType;
-            this.volunteers = new VolunteerGeneric<String, Integer>(volunteerId, tempInt);
+            this.volunteerOne = new VolunteerGeneric<String, Integer>(volunteerId, tempInt);
             this.animalTaskFor = animal;
         }
 
