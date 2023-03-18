93c35aaa15bdde698528791bacc8874f663ca0c0
karambaroud <113051905+karambaroud@users.noreply.github.com>
2023-03-18 15:48:06 -0600

    Added a rough draft of the writeToSchedule method
    converts the string segment into a charArray and writes each character one by one to the schedule file. Requires that segment be formatted properly.
    Here's an example from the Project handout of how "segment" could be formatted:
    
    19:00
    * Rebandage leg wound (Slinky)
    * Kit feeding (Annie, Oliver and Mowgli)
    * Feeding - porcupine (2: Spike, Javelin)

--- ENSF380FinalProject/src/edu/ucalgary/oop/PrintLog.java
+++ ENSF380FinalProject/src/edu/ucalgary/oop/PrintLog.java
@@ -1,11 +1,40 @@
 package edu.ucalgary.oop;
 
+import java.io.*;
+
 public class PrintLog {
     public PrintLog() {
 
     }
 
     public void writeToSchedule(String segment) {
+        FileWriter schedule = null;
+        String fileName = "schedule.txt";
+        char[] charArray = segment.toCharArray();
 
+        try {
+            schedule = new FileWriter(fileName, true);
+
+            for (char c : charArray) {
+                schedule.write(c);
+            }
+
+        } 
+        catch (IOException e) {
+            System.out.println("Error writing to file " + fileName);
+            e.printStackTrace();
+        }
+
+        finally {
+            if (schedule != null) {
+                try {
+                    schedule.close();
+                }
+                catch (IOException e) {
+                    System.out.println("Couldn't close file " + fileName);
+					e.printStackTrace();
+                }
+            }
+        }
     }
 }
