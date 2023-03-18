a48062f811899a34d2a303c722afff7be0276586
karambaroud <113051905+karambaroud@users.noreply.github.com>
2023-03-18 16:41:40 -0600

    Added documentation to PrintLog class

--- ENSF380FinalProject/src/edu/ucalgary/oop/PrintLog.java
+++ ENSF380FinalProject/src/edu/ucalgary/oop/PrintLog.java
@@ -1,12 +1,30 @@
+/**
+@author     Group5
+@version    1.2
+@since      1.0
+*/
+
+/*
+ * PrintLog is a class which writes to the schedule.txt file.
+*/
+
 package edu.ucalgary.oop;
 
 import java.io.*;
 
 public class PrintLog {
+
+    /**
+     * This is the constructor for the PrintLog class. It has no parameters.
+     */
     public PrintLog() {
 
     }
 
+    /**
+     * This method writes to the schedule.txt file.
+     * @param segment   The string segment to be written to the file. This segement must be formatted before being passed to this method.
+     */
     public void writeToSchedule(String segment) {
         FileWriter schedule = null;
         String fileName = "schedule.txt";
