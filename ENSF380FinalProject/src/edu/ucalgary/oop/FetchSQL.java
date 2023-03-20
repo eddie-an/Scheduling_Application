15139d3f34b2f137889b430cadf971b026fcafb1
Alphamale42 <111829951+Alphamale42@users.noreply.github.com>
2023-03-20 17:47:10 -0600

    Update FetchSQL.java

--- ENSF380FinalProject/src/edu/ucalgary/oop/FetchSQL.java
+++ ENSF380FinalProject/src/edu/ucalgary/oop/FetchSQL.java
@@ -6,6 +6,7 @@ public class FetchSQL {
 
     private Connection dbConnection;
     private ResultSet results;
+    private SinglyLinkedList storage = new SinglyLinkedList();
 
     public FetchSQL() {
 
@@ -88,3 +89,56 @@ public class FetchSQL {
 
     }
 }
+
+class SinglyLinkedList {    
+    //Represent a node of the singly linked list    
+    class Node{    
+        int data;    
+        Node next;    
+            
+        public Node(int data) {    
+            this.data = data;    
+            this.next = null;    
+        }    
+    }    
+     
+    //Represent the head and tail of the singly linked list    
+    public Node head = null;    
+    public Node tail = null;    
+        
+    //addNode() will add a new node to the list    
+    public void addNode(int data) {    
+        //Create a new node    
+        Node newNode = new Node(data);    
+            
+        //Checks if the list is empty    
+        if(head == null) {    
+            //If list is empty, both head and tail will point to new node    
+            head = newNode;    
+            tail = newNode;    
+        }    
+        else {    
+            //newNode will be added after tail such that tail's next will point to newNode    
+            tail.next = newNode;    
+            //newNode will become new tail of the list    
+            tail = newNode;    
+        }    
+    }    
+    
+      public void display() {    
+        //Node current will point to head    
+        Node current = head;    
+            
+        if(head == null) {    
+            System.out.println("List is empty");    
+            return;    
+        }    
+        System.out.println("Nodes of singly linked list: ");    
+        while(current != null) {    
+            //Prints each node by incrementing pointer    
+            System.out.print(current.data + " ");    
+            current = current.next;    
+        }    
+        System.out.println();    
+    }   
+        
