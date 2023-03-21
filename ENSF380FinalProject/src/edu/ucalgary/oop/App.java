aa4b7b50cf4d93023c543f965a0e90d663dbdfec
karambaroud <113051905+karambaroud@users.noreply.github.com>
2023-03-20 19:08:28 -0600

    Added a limited GUI
    GUI has a button to start schedule building. Then in action performed, it shows a "Confirm Backup Volunteer" button, still needs a try/catch block to resume the program after button press.

--- ENSF380FinalProject/src/edu/ucalgary/oop/App.java
+++ ENSF380FinalProject/src/edu/ucalgary/oop/App.java
@@ -21,16 +21,68 @@
 */
 
 package edu.ucalgary.oop;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class App {
+public class App implements ActionListener {
 
     /**
      * This is the main method of the program.
-     * @param args  The command line arguments.
+     * @param args          The command line arguments.
      * @throws Exception    If an exception is thrown.
      */
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
-}
+        EventQueue.invokeLater(() -> {
+            JFrame frame = new JFrame(" Example Wildlife Rescue Scheduler");
+            frame.setSize(400, 400);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            
+            JPanel buttonsPanel = new JPanel();
+            JButton myButton = new JButton("Build Schedule Based On Database");
 
+            App buttonListener = new App();
+            myButton.addActionListener(buttonListener);
+            buttonsPanel.add(myButton);
+            frame.getContentPane().add(BorderLayout.NORTH, buttonsPanel);
+            frame.setVisible(true);
+
+        });
+    }
+
+    public void actionPerformed(ActionEvent event){
+
+        //Here call the method that will initiate the schedule building process
+        //Exceptions which require calling in a backup volunteer should be handled here
+
+        //add try block that calls the method that builds the schedule
+
+
+        //add catch block that confirms backup volunteer and makes changes accordingly to the schedule
+
+        JPanel backupPanel = new JPanel();
+        JButton backupButton = new JButton("Confirm Backup Volunteer");
+
+        backupButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+
+                JOptionPane.showMessageDialog(null, "Backup volunteer confirmed");
+
+                // Remove the backup button from its parent container
+                backupPanel.remove(backupButton);
+                backupPanel.revalidate();
+                backupPanel.repaint();
+            }
+        });
+
+        backupPanel.add(backupButton);
+        JFrame frame = (JFrame) SwingUtilities.getWindowAncestor((Component) event.getSource());
+        frame.getContentPane().add(BorderLayout.SOUTH, backupPanel);
+        frame.pack();
+
+        JButton myButton = (JButton) event.getSource();
+        myButton.setVisible(false);
+    }
+
+}
